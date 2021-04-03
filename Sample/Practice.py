9ca5856f0e4eb233e6b5906cebf3b1c1d3e9cea9
venkateshlr56 <venkateshlr56@gmail.com>
2021-04-03 09:51:51 +0530

    first push

---
+++ Sample/Practice.py
@@ -0,0 +1,58 @@
+# a=(10,20,30,40,50,10,30)
+# print(a)
+# for i in a:
+#     print(i)
+#
+# for i,j in enumerate(a):
+#     print(i,'->',j)
+
+# print(a.count(10))
+# print(max(a))
+# print(min(a))
+
+# print(len(a))
+# print(a.index(20))
+# print(a.index(10,2))
+# print(a.index(10,3,6))
+
+
+# print(a)
+# print(type(a))
+#
+# print(a[2:4])
+# print(a[0:])
+# print(a[:3])
+# print(a[:])
+# print(a[-4:-1])
+# print(a[:-2])
+# print(a[-3:])
+
+
+
+
+
+
+a={10,20,30,40,50,20,40}
+print(a)
+print(type(a))
+print(len(a))
+a.add(60)
+print(a)
+print(a.update())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
